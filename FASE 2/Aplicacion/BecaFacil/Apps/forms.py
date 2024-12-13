b21cac158fe30d52e91e3a0d99bba2bc1c795961
MarcosCeballos <156121266+DexterGriffings@users.noreply.github.com>
2024-12-13 13:05:49 -0300

    Delete FASE 2/Aplicacion/BecaFacil directory

--- FASE 2/Aplicacion/BecaFacil/Apps/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Cuestionario
-
-class CuestionarioForm(forms.ModelForm):
-    class Meta:
-        model = Cuestionario
-        fields = ['nombre', 'rut', 'correo', 'telefono', 'colegio']
